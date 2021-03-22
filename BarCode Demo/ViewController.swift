d482e5f686c29c44b9c6a7f2465906d18173e2eb
simon wilson <simonhanwilson@hotmail.co.uk>
2021-03-22 22:20:47 +0000

    test

--- BarCode Demo/ViewController.swift
+++ BarCode Demo/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import MLKitBarcodeScanning
+import MLKitVision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let barcodeOptions = BarcodeScannerOptions(formats: .all)
+        
+        let visionImage = VisionImage(image: UIImage())
+        
+        visionImage.orientation = .up
+        
+        let barcodeScanner = BarcodeScanner.barcodeScanner(options: barcodeOptions)
+        
+        barcodeScanner.process(visionImage) { (barcodes, error) in
+            
+            if error != nil {
+                
+                
+                
+            } else {
+                
+                if let barcodeArray = barcodes {
+                    
+                    for barcode in barcodeArray {
+                        
+                        print(barcode.rawData)
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+       
+        
+        
     }
 
 
